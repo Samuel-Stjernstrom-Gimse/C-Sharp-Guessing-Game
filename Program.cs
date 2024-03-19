05a4015613577d1554a875bf068f35cf3d1d4669
Zam <samuel.kodehode@gmail.com>
2024-03-19 10:31:45 +0100

    inti

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var random = new Random();
+            var randomNumber = random.Next(0, 101);
+            int guess;
+            var counter = 0;
+            
+            Console.WriteLine("guess a number between 0-100");
+            
+            do
+            {
+                guess = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+                counter++;
+                if (guess == randomNumber)
+                {
+                    Console.WriteLine($"Correct!! the Number is  {randomNumber} :) you used {counter} tries");
+                }
+                else
+                {
+                    Console.WriteLine(guess < randomNumber ? "number bigger that that" : "number is smaller than that");
+                }
+            } while (guess != randomNumber);
+            
+            Console.ReadLine();
+        }
+    }
+}
